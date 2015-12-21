e3e11495219d79c7b72d09c11b88c7b1bf4b5cea
smart__petea <smart__petea@mail.ru>
2015-12-21 22:07:19 +0200

    mnogochlen.cpp

--- mnogochlen.cpp
+++ mnogochlen.cpp
@@ -22,33 +22,50 @@ bool getSign(char& character, std::istream& is, int& sign)
             is.get(character);
             break;
         default:
+            sign = 1;
             break;
     }
+
     return true;
 }
 
 bool getCoefficient(char& character, std::istream& is, int& coefficient)
 {
-    coefficient = 0;
-    while(character >= '0' && character <= '9')
+    if(character >= '0' && character <= '9')
     {
-        coefficient = coefficient * 10 + character;
+        coefficient = 0;
+        while(character >= '0' && character <= '9')
+        {
+            coefficient = coefficient * 10 + (character - '0');
+            is.get(character);
+        }
+
+        if(character == '*') is.get(character); //?
+    } else 
+    {
+        coefficient = 1;
     }
 
     return true;
 }
 
-bool preparePower(char& character, std::istream& is)
+bool preparePower(char& character, std::istream& is, int& power)
 {
-    if(character != '*') return false;
+    if(character != 'x') 
+    {
+        power = 0;
+        return false;
+    }
 
     is.get(character);
-    if(character != 'x') return false;
+    if(character != '^') 
+    {
+        power = 1;
+        return false;
+    }
 
     is.get(character);
-    if(character != '^') return false;
 
-    is.get(character);
     return true;
 }
 
@@ -56,28 +73,55 @@ bool preparePower(char& character, std::istream& is)
 int main()
 {
     char character;
-    int power;
+    int pwr;
     int coefficient;
     int sign;
     std::map<int, int> polinom;
 
-    power = 1;
-    coefficient = 1;
-    while((character = std::cin.get()) != '=')
+    character = std::cin.get();
+    while(character != '\n')
     {
+
         getSign(character, std::cin, sign)
         &&
         getCoefficient(character, std::cin, coefficient)
         &&
-        preparePower(character, std::cin)
+        preparePower(character, std::cin, pwr)
         &&
-        getCoefficient(character, std::cin, power);
+        getCoefficient(character, std::cin, pwr);
 
         coefficient *= sign;
 
-        polinom[power] += coefficient;
-
+        polinom[pwr] += coefficient;
     }
+
+    std::map<int, int>::iterator it = polinom.begin();
+    std::map<int, int>::iterator end = polinom.end();
+    bool isFirstLoop = true;
+    for(;it != end; it++)
+    {
+        pwr = it->first - 1;
+        coefficient = it->second * it->first;
+
+        if(coefficient > 0) {
+            if(isFirstLoop == false) std::cout << "+";
+        } else if (coefficient < 0)
+        {
+            std::cout << "-";
+        } else
+        {
+            continue;
+        }
+
+        std::cout << coefficient;
+        if(pwr > 0) {
+            std::cout << "*x";
+            if(pwr > 1) std::cout << "^" << pwr;
+        }
+
+        isFirstLoop = false;
+    }
+    std::cout << std::endl;
 
     return 0;
 }
