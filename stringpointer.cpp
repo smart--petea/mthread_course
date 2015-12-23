2703d0bac39c18d7694baaf1aad20e3218ebb17e
smart__petea <smart__petea@mail.ru>
2015-12-23 20:07:06 +0200

    stringpointer

--- stringpointer.cpp
+++ stringpointer.cpp
@@ -1,32 +1,49 @@
 #include <iostream>
 #include <string>
 
+#include <iostream>
+#include <string>
+
 class StringPointer
 {
-    public:
-        StringPointer() : ptr(0), homeMade(false) {}
-        StringPointer(std::string* ptr): ptr(ptr), homeMade(false) {}
-
-        std::string* operator->()
-        {
-            if(ptr == 0) 
+        public:
+            StringPointer(std::string* ptr): ptr(ptr), raw(false) {}
+            std::string* operator->()
             {
-                homeMade = true;
-                ptr = new std::string;
+                checkPtr();
+                return ptr;
             }
 
-            return ptr;
-        }
+            std::string operator*()
+            {
+                checkPtr();
+                return *ptr; 
+            }
 
-        operator std::string*() { return ptr; }
-        ~StringPointer()
-        {
-            if(homeMade) delete ptr;
-        }
+            operator std::string*() 
+            { 
+                checkPtr();
+                return ptr; 
+            }
+            ~StringPointer()
+            {
+                if(raw)
+                {
+                    delete ptr;
+                }
+            }
 
-    private:
-        std::string* ptr;
-        bool homeMade;
+            private:
+                void checkPtr()
+                {
+                    if(ptr == NULL) 
+                    {
+                        raw = true;
+                        ptr = new std::string();
+                    }
+                }
+                std::string* ptr;
+                bool raw;
 };
 
 int main()
@@ -36,9 +53,9 @@ int main()
     StringPointer sp1(&s1);
     StringPointer sp2(NULL);
 
-    std::cout << sp1->length() << std::endl;
-    std::cout << *sp1 << std::endl;
-    std::cout << sp2->length() << std::endl;
-    std::cout << *sp2 << std::endl;
+    std::cout << "sp1->length(): " << sp1->length() << std::endl;
+    std::cout << "*sp1: " << *sp1 << std::endl;
+    std::cout << "sp2->length: " << sp2->length() << std::endl;
+    std::cout << "*sp2: " << (*sp2).length() << std::endl;
     return 0;
 }
